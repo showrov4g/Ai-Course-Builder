dfcf868a089159d9c51023bf9b24a1cd60574212
showrov4g <showrovg00@gmail.com>
2025-03-15 10:48:24 +0600

    making authantication

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -3,37 +3,32 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <section className="bg-gray-50">
-  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-    <div className="mx-auto max-w-xl text-center">
-      <h1 className="text-3xl font-extrabold sm:text-5xl">
-        Understand User Flow.
-        <strong className="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
-      </h1>
+      <section className="bg-gray-50">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
+            <h1 className="text-3xl font-extrabold sm:text-5xl text-purple-500">
+              Ai course generator
+              <strong className="font-extrabold text-black sm:block"> Custom learning path with AI </strong>
+            </h1>
 
-      <p className="mt-4 sm:text-xl/relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-        numquam ea!
-      </p>
+            <p className="mt-4 sm:text-xl/relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+              numquam ea!
+            </p>
 
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <a
-          className="block w-full rounded-sm bg-red-600 px-12 py-3 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:ring-3 focus:outline-hidden sm:w-auto"
-          href="#"
-        >
-          Get Started
-        </a>
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded-sm bg-purple-500 px-12 py-3 text-sm font-medium text-white shadow-sm hover:bg-purple-600 focus:ring-3 focus:outline-hidden sm:w-auto"
+                href="#"
+              >
+                Get Started
+              </a>
 
-        <a
-          className="block w-full rounded-sm px-12 py-3 text-sm font-medium text-red-600 shadow-sm hover:text-red-700 focus:ring-3 focus:outline-hidden sm:w-auto"
-          href="#"
-        >
-          Learn More
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
+
+            </div>
+          </div>
+        </div>
+      </section>
 
     </div>
   )
