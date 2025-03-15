90a6ec1824ca61f41e6d5b5f5a9a6df557c6010e
showrov4g <showrovg00@gmail.com>
2025-03-15 09:56:11 +0600

    header section design start

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <Image src={"/logo.png"} width={200} height={200} alt='Logo_image'></Image>
+    </div>
   )
 }
 
