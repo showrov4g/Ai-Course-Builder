58f3803fad3e80b04a32bc6e57507bc63c21621b
showrov4g <showrovg00@gmail.com>
2025-03-17 09:14:07 +0600

    making course section

--- app/dashboard/_components/AddCourse.jsx
+++ app/dashboard/_components/AddCourse.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const AddCourse = () => {
     const {user} = useUser();
   return (
-    <div>
+    <div className='flex items-center justify-between'>
         <div>
             <h2 className='text-2xl'>Hello, <span className='font-bold'>{user?.fullName}</span></h2>
             <p>Create a course with AI and learn something more</p>
