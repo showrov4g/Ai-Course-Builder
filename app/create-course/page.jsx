5bfe6b9632eb891bc0fa365b63c4dbb954e301a9
showrov4g <showrovg00@gmail.com>
2025-03-17 21:17:02 +0600

    dascboard page design

--- app/create-course/page.jsx
+++ app/create-course/page.jsx
@@ -7,6 +7,21 @@ const stepperOptions = [
         id: 1,
         name: 'category',
         icons: <SiElasticstack />,
+    },
+    {
+        id: 2,
+        name: 'category',
+        icons: <SiElasticstack />,
+    },
+    {
+        id: 3,
+        name: 'category',
+        icons: <SiElasticstack />,
+    },
+    {
+        id: 4,
+        name: 'category',
+        icons: <SiElasticstack />,
     }
 ]
 
@@ -16,6 +31,21 @@ const stepperOptions = [
             <div>
                 <div className='flex flex-col items-center justify-between'>
                     <h2 className='text-4xl text-purple-500 font-medium'>Create course</h2>
+                    <div >
+                     {
+                            stepperOptions.map((item) => (
+                                <div key={item.id} className='flex items-center justify-between'>
+                                    <div className='flex items-center'>
+                                        <div className='flex items-center justify-center w-10 h-10 bg-purple-500 rounded-full'>
+                                            {item.icons}
+                                        </div>
+                                        <h3 className='text-xl font-medium ml-2'>{item.name}</h3>
+                                    </div>
+                                    <div className='w-1 h-1 bg-purple-500'></div>
+                                </div>
+                            ))
+                     }
+                    </div>
                 </div>
             </div>
         {/* components  */}
